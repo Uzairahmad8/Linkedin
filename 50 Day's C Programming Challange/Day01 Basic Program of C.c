791a62be0c3ecfd718843532849b1517b5d5f0e6
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-02-06 14:29:34 +0500

    Basic Program of C.

---
+++ 50 Day's C Programming Challange/Day01 Basic Program of C.c
@@ -0,0 +1,15 @@
+// Author = Uzair Ahmad
+
+#include<stdio.h>
+
+int main(){
+		
+  	 int number ; // declaration of varaible ;
+  	 
+  	 printf("Enter Number : ");
+  	 scanf("%d", &number);
+  	 
+  	 printf("You Entered %d ", number);
+	
+	return 0;
+}
