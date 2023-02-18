994170a78ddd1e81809301852c80df08f0040bfa
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-02-19 02:44:16 +0500

    Day 15

---
+++ 50 Day's C Programming Challange/Day15 Greater number amoung two.c
@@ -0,0 +1,22 @@
+// Author = Uzair Ahmad ;
+
+#include <stdio.h>
+
+int main() {
+
+    int number_one = 5 , number_two = 8 ;
+    
+  	// Condition 
+    if ( number_one > number_two )
+    {
+        printf("%d is Greater", number_one );      
+    }
+    else 
+    {
+        printf("%d is Greater", number_two );            
+    }
+    
+
+    return 0;
+}
+
